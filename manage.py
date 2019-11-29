33feffee08fa7c70e51cdc3e06aa93525695c90f
gradrix <grazjuc@gmail.com>
2019-11-29 10:46:32 +0200

    after-refactor changes from root directory

--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.7
 import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "leds_web.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
--- web/urls.py
+++ web/urls.py
@@ -5,8 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^api/', include('ledcontrol.urls')),
-#    url(r'^api/', include(ledApi.urls)),
+    url(r'^api/', include('api.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='index.html'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
